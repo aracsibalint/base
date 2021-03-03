67d80f5654350e1cd10d136ca0dfcad6753f7594
aracsibalint <balint.aracsi@gmail.com>
2021-03-03 23:58:32 +0100

    JUnit

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -7,13 +7,18 @@ import static org.mockito.Mockito.*;
 
 public class TrainSensorTest {
 
+	int a, b;
     @Before
     public void before() {
         // TODO Add initializations
+    	a = 3;
+    	b = 5;
     }
 
     @Test
     public void ThisIsAnExampleTestStub() {
         // TODO Delete this and add test cases based on the issues
+    	Assert.assertEquals(3, a);
+    	Assert.assertEquals(5, b);
     }
 }
